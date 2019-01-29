5695dad96191fec8308541c7817c68fe4a49e3ac
brunabkrebs <47150255+brunabkrebs@users.noreply.github.com>
2019-01-29 11:48:50 -0500

    Update Ca analysis after Suite2p - 01.15.2019.m

--- Ca analysis after Suite2p - 01.15.2019.m
+++ Ca analysis after Suite2p - 01.15.2019.m
@@ -1,11 +1,8 @@
-%% 
 % *Clear Workspace*
-
 clear all
 clc
-%% 
-% *Import Data*
 
+% *Import Data*
 %Import Deconvolved file
 File = uigetfile('*.xlsx');
 PreData = xlsread(File);
@@ -17,21 +14,18 @@ cells = IsCell==1;
 PreData = PreData(cells, :);
 Data = PreData';
 [NumberofRows, NumberofColumns] = size(Data);
-%% 
+ 
 % *Find Spikes*
-
 for i=1:NumberofColumns
     figure(i)
     [amplitudes{i},locations{i}, widths{i}, prominences{1}] = findpeaks(Data(:,i));
     findpeaks(Data(:,i), 'Annotate', 'extents')
 end   
-%% 
+ 
 % *Frequency of spikes*
-
 %Change duration accordingly
 Duration = duration(00,04,00);
 %or for seconds, min*60 (for frequency in Hz)
-
 
 %Change duration accordingly
 for i = 1:NumberofColumns;
@@ -40,9 +34,8 @@ end
 
 Frequency = cell2mat(Frequency);
 Frequency = Frequency';
-%% 
+ 
 % *Mean interval between spikes*
-
 %Shows the mean of intervals between peaks 
 for i = 1:NumberofColumns;
     Interval{i} = mean(diff(cell2mat(locations(1,i))));
@@ -52,12 +45,10 @@ Interval=cell2mat(Interval);
 Interval=Interval';
 Interval_in_seconds=Interval./15;
 
-
 %Shows distribution of peak intervals
 hist(Interval)
-%% 
+ 
 % *Plot correlation matrix and find average of correlation coefficient*
-
 [rho, pval] = corr(Data, 'rows', 'pairwise');
 imagesc(rho)
 colormap('jet')
@@ -65,9 +56,8 @@ colorbar
 
 corrcoef=nonzeros(tril(rho,-1));
 meanofcorrcoef=mean(corrcoef)
-%% 
+ 
 % *Sort matrix by similarity for a better correlation matrix*
-
 %for this to work, each cell must be a row
 %Max number of clusters can be adaptable
 Data2 = Data';
@@ -87,9 +77,8 @@ colorbar
 
 corrcoef2=nonzeros(tril(rho2,-1));
 meanofcorrcoef2=mean(corrcoef2)
-%% 
+ 
 % *Write results to Excel file*
-
 %Fill arrays with NaN for calculations
 for i=1:NumberofColumns;
     amplitudes{1,i}(end+1:NumberofRows)=nan;
@@ -98,13 +87,12 @@ for i=1:NumberofColumns;
     prominences{1,1}(end+1:NumberofRows)=nan;
 end
 
-%%
 %Transform arrays into matrixes
 amplitudes=cell2mat(amplitudes);
 locations=cell2mat(locations);
 widths=cell2mat(widths);
 prominences=cell2mat(prominences);
-%%
+
 %Write and save results in individual tables
 xlswrite('amplitudes.xlsx', amplitudes);
 xlswrite('peak_locations.xlsx', locations);
