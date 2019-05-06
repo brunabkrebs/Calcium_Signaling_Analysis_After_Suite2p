ced000f6b2c2637bdd2b7b7c70be159bdbc41899
Bruna Krebs Kutche <47150255+brunabkrebs@users.noreply.github.com>
2019-05-06 14:41:16 -0400

    Add files via upload
    Added average amplitude per cell, and synchronization plots

--- Ca analysis after Suite2p - 01.15.2019.m
+++ Ca analysis after Suite2p - 01.15.2019.m
@@ -1,8 +1,11 @@
+%% 
 % *Clear Workspace*
+
 clear all
 clc
-
+%% 
 % *Import Data*
+
 %Import Deconvolved file
 File = uigetfile('*.xlsx');
 PreData = xlsread(File);
@@ -14,90 +17,133 @@ cells = IsCell==1;
 PreData = PreData(cells, :);
 Data = PreData';
 [NumberofRows, NumberofColumns] = size(Data);
- 
+fps = round(str2double(inputdlg('What is the FPS?')));
+duration = str2double(inputdlg('What is the duration in minutes?'))
+%% 
 % *Find Spikes*
+
 for i=1:NumberofColumns
     figure(i)
-    [amplitudes{i},locations{i}, widths{i}, prominences{1}] = findpeaks(Data(:,i));
+    [amplitudes{i},locations{i}, ~, ~] = findpeaks(Data(:,i));
     findpeaks(Data(:,i), 'Annotate', 'extents')
 end   
- 
-% *Frequency of spikes*
-%Change duration accordingly
-Duration = duration(00,04,00);
-%or for seconds, min*60 (for frequency in Hz)
+%% 
+% *Amplitudes*
 
-%Change duration accordingly
+%Calculates the average amplitudes per cell
+
 for i = 1:NumberofColumns;
-    Frequency{i} = length(locations{1,i})/240;
+    AvgAmplitudes{i} = mean(amplitudes{1,i});
+end
+
+AvgAmplitudes = cell2mat(AvgAmplitudes);
+AvgAmplitudes = AvgAmplitudes';
+%% 
+% *Frequency of spikes (Hz)*
+
+for i = 1:NumberofColumns;
+    Frequency{i} = length(locations{1,i})/(60*duration);
 end
 
 Frequency = cell2mat(Frequency);
 Frequency = Frequency';
- 
-% *Mean interval between spikes*
-%Shows the mean of intervals between peaks 
+%% 
+% *Interspike Interval*
+
 for i = 1:NumberofColumns;
-    Interval{i} = mean(diff(cell2mat(locations(1,i))));
+    Interval{i} = (diff(cell2mat(locations(1,i))))./fps;
+end
+%%
+%Calculates the mean of interspike intervals 
+for i = 1:NumberofColumns;
+    MeanInterval{i} = mean(diff(cell2mat(locations(1,i))))./fps;
 end
 
-Interval=cell2mat(Interval);
-Interval=Interval';
-Interval_in_seconds=Interval./15;
+%Change fps accordingly
+MeanInterval=cell2mat(MeanInterval);
+MeanInterval=MeanInterval';
 
 %Shows distribution of peak intervals
-hist(Interval)
- 
+hist(MeanInterval)
+%% 
 % *Plot correlation matrix and find average of correlation coefficient*
-[rho, pval] = corr(Data, 'rows', 'pairwise');
-imagesc(rho)
-colormap('jet')
-colorbar
 
-corrcoef=nonzeros(tril(rho,-1));
-meanofcorrcoef=mean(corrcoef)
- 
+% [rho, pval] = corr(Data, 'rows', 'pairwise');
+% imagesc(rho)
+% colormap('jet')
+% colorbar
+% 
+% corrcoef=nonzeros(tril(rho,-1));
+% meanofcorrcoef=mean(corrcoef)
+%% 
 % *Sort matrix by similarity for a better correlation matrix*
-%for this to work, each cell must be a row
-%Max number of clusters can be adaptable
-Data2 = Data';
-cluster = clusterdata(Data2, 'maxclust', 6);
-Data2 = [cluster Data2];
-Data2 = sortrows(Data2);
 
-%De-transform rows to columns again
-Data2 = Data2(:, 2:end);
-Data2 = Data2';
+% %for this to work, each cell must be a row
+% %Max number of clusters can be adaptable
+% Data2 = Data';
+% cluster = clusterdata(Data2, 'maxclust',3);
+% Data2 = [cluster Data2];
+% Data2 = sortrows(Data2);
+% 
+% %De-transform rows to columns again
+% Data2 = Data2(:, 2:end);
+% Data2 = Data2';
+% 
+% %run correlation again
+% [rho2, pval2] = corr(Data2, 'rows', 'pairwise');
+% imagesc(rho2)
+% colormap('jet')
+% colorbar
+% 
+% corrcoef2=nonzeros(tril(rho2,-1));
+% meanofcorrcoef2=mean(corrcoef2)
+%% 
+% *Transform arrays into matrixes*
 
-%run correlation again
-[rho2, pval2] = corr(Data2, 'rows', 'pairwise');
-imagesc(rho2)
-colormap('jet')
-colorbar
-
-corrcoef2=nonzeros(tril(rho2,-1));
-meanofcorrcoef2=mean(corrcoef2)
- 
-% *Write results to Excel file*
 %Fill arrays with NaN for calculations
 for i=1:NumberofColumns;
     amplitudes{1,i}(end+1:NumberofRows)=nan;
     locations{1,i}(end+1:NumberofRows)=nan;
-    widths{1,i}(end+1:NumberofRows)=nan;
-    prominences{1,1}(end+1:NumberofRows)=nan;
+    %widths{1,i}(end+1:NumberofRows)=nan;
+    %prominences{1,1}(end+1:NumberofRows)=nan;
+    Interval{1,i}(end+1:NumberofRows)=nan;
 end
 
 %Transform arrays into matrixes
 amplitudes=cell2mat(amplitudes);
 locations=cell2mat(locations);
-widths=cell2mat(widths);
-prominences=cell2mat(prominences);
+%widths=cell2mat(widths);
+%prominences=cell2mat(prominences);
+Interval=cell2mat(Interval);
+%% 
+% *Synchronization plots*
+
+matrix=zeros(NumberofRows,NumberofColumns);
+for i=1:NumberofColumns;
+    matrix(:,i)=i;
+end
+
+plot(locations,matrix,'*')
+
+axis([0 NumberofRows 0 (NumberofColumns+1)]);
+title('Synchronization');
+xlabel('Frame');
+ylabel('Cell Number')
+
+%%
+% %Plot some plots only:
+% plot(locations(:,1),matrix(:,1),'*')
+% hold on
+% plot(locations(:,2),matrix(:,2),'*')
+%% 
+% *Write results to Excel file*
 
 %Write and save results in individual tables
-xlswrite('amplitudes.xlsx', amplitudes);
-xlswrite('peak_locations.xlsx', locations);
-xlswrite('widths.xlsx', widths);
-xlswrite('prominences.xlsx', prominences);
-xlswrite('interval_between_peaks.xlsx', Interval);
-xlswrite('peak_frequency.xlsx', Frequency);
-xlswrite('Interval_in_seconds.xlsx', Interval_in_seconds);
+xlswrite('Amplitudes.xlsx', amplitudes);
+xlswrite('Peak_locations.xlsx', locations);
+%xlswrite('widths.xlsx', widths);
+%xlswrite('prominences.xlsx', prominences);
+xlswrite('Average_amplitudes.xlsx', AvgAmplitudes);
+xlswrite('Frequency.xlsx', Frequency);
+xlswrite('Interspike_interval.xlsx', Interval);
+xlswrite('Average_Interspike_interval.xlsx', Mean_Interval);
